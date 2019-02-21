c210d4116588125f0181df0c892098016c3b2efc
yonghyunkim <sociablesasha@gmail.com>
2019-02-21 10:23:04 +0900

    ToDoListTemplate

---
+++ src/components/ToDoListTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ToDoListTemplate.css';
+
+const ToDoListTemplate = ({ form, children }) => {
+    return (
+        <main className="todo-list-template">
+            <div className="title">To Do List</div>
+            <section className="form-wrapper">{form}</section>
+            <section className="todos-wrapper">{children}</section>
+        </main>
+    );
+};
+
+export default ToDoListTemplate;
